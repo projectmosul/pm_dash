b690ee181dc6e00be03dbacbe7abb4d87826fc5d
Matthew Vincent <matt@averails.com>
2015-04-02 09:48:05 +0200

    certificate gem updates

--- jobs/github_issues.rb
+++ jobs/github_issues.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 require 'rest-client'
 require 'json'
 require 'date'
@@ -15,18 +14,6 @@ event_name = "git_issues_labeled_defects"
 uri = "https://api.github.com/repos/#{git_owner}/#{git_project}/issues?state=open&per_page=100&access_token=#{git_token}"
 
  
-## Create an array to hold our data points
-points = []
- 
-## One hours worth of data for, seed 60 empty points (rickshaw acts funny if you don't).
-(0..60).each do |a|
-  points << { x: a, y: 0.01 }
-end
- 
-## Grab the last x value
-last_x = points.last[:x]
- 
- 
 SCHEDULER.every '1h', :first_in => 0 do |job|
     puts "Getting #{uri}"
     response = RestClient.get uri
@@ -34,15 +21,7 @@ SCHEDULER.every '1h', :first_in => 0 do |job|
  
     current_defects = issues.length
  
-    ## Drop the first point value and increment x by 1
-    points.shift
-    last_x += 1
- 
-    ## Push the most recent point value
-    points << { x: last_x, y: current_defects  }
- 
-    send_event(event_name, {
-            text: current_defects, points:points
-        })
+    send_event('git_issues_labeled_defects', { current: current_defects })
+
  
 end # SCHEDULER
